21c1b3ef996cff36f72e377f40ad376297393d81
KarthickKottaisamy <108003499+KarthickKottaisamy@users.noreply.github.com>
2024-11-14 19:01:45 +0530

    Add files via upload

---
+++ Interview_Question/swapNumbers.java
@@ -0,0 +1,17 @@
+package Interview_Question;
+
+public class swapNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 7;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println("a= "+a);
+		System.out.println("b= "+b);
+
+	}
+
+}
